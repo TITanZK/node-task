16afabd5665ff58564a9fc38ea0fdb57d0e5d100
TITanZK <503838819@qq.com>
2021-12-30 11:15:49 +0800

    完成选中任务

--- index.js
+++ index.js
@@ -29,7 +29,49 @@ module.exports = {
         choices: [{ name: 'quit', value: '-1' }, ...choicesArray, { name: '+ create a task', value: '-2' }]
       })
       .then((answers) => {
-        console.log(answers);
+        const index = parseInt(answers.index)
+        if (index >= 0) {
+          inquirer.prompt({
+            type: 'list',
+            name: 'action',
+            message: 'Please select your action',
+            choices: [
+              { name: '退出', value: 'quit' },
+              { name: '已完成', value: 'markAsDone' },
+              { name: '未完成', value: 'markAsUnDone' },
+              { name: '修改标题', value: 'updateTitle' },
+              { name: '删除', value: 'remove' }
+            ]
+          }).then(answers2 => {
+            switch (answers2.action) {
+              case 'markAsDone':
+                list[index].done = true
+                db.write(list)
+                break;
+              case 'markAsUnDone':
+                list[index].done = false
+                db.write(list)
+                break;
+              case 'updateTitle':
+                inquirer.prompt({
+                  type: 'input',
+                  name: 'title',
+                  message: "New title",
+                  default: list[index].title
+                }).then(answers3 => {
+                  list[index].title = answers3.title
+                  db.write(list)
+                });
+                break;
+              case 'remove':
+                list.splce(index, 1)
+                db.write(list)
+                break;
+            }
+          })
+        } else if (index === -2) {
+
+        }
       });
   }
 }
